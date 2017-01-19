57748a60864fc70934c80f7a1be68dcd352d1d9b
ecoad <elliot.coad@shortlist.com>
2017-01-19 18:04:36 +0000

    Retrieve the Post as JSON add it to the document

--- agreable-tapestry-plugin.php
+++ agreable-tapestry-plugin.php
@@ -12,9 +12,56 @@
 */
 class AgreableTapestryPlugin
 {
-    public function __construct()
-    {
-        //
-    }
+		public function __construct()
+		{
+			add_action('wp', [$this, 'on_wp_action']);
+		}
+
+		public function on_wp_action()
+		{
+			global $post;
+
+			if (!$post || !isset($post->post_status) || $post->post_status === 'publish') {
+				return; // At the moment we are only interested in non-published posts
+			}
+
+			// To allow booting of Tapestry we must add the JSON to the document
+			// for JavaScript to access
+			$this->add_post_json_to_document($post);
+		}
+
+		public function add_post_json_to_document($post)
+		{
+			$request = new WP_REST_Request('GET', "/wp/v2/posts/" . $post->ID);
+			$response = rest_get_server()->dispatch($request);
+			if ($response->status !== 200) {
+				echo "Error retrieving Post from API (HTTP . {$response->status})";
+				exit;
+			}
+
+			// TODO: Cleaner implementation
+			echo "<!doctype html><head>";
+
+			wp_head();
+
+			echo "</head><body>";
+
+			echo "<script>window.tapestryPost = " . json_encode($response->data) . "</script>";
+
+			$this->boot_tapestry();
+
+			wp_footer();
+
+			echo "</body>";
+
+			exit; // TODO: Is there a better way of forcing the Theme not to load?
+		}
+
+		public function boot_tapestry()
+		{
+			echo "Tapestry loading&hellip;";
+
+			//TODO Boot up the Tapestry client
+		}
 }
 new AgreableTapestryPlugin();
