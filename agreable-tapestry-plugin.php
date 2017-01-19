efab24ee3a725b90e516a401f451da3963bdb119
ecoad <elliot.coad@shortlist.com>
2017-01-19 16:21:24 +0000

    init

---
+++ agreable-tapestry-plugin.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: agreable-tapestry-plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-tapestry-plugin
+* Description: WordPress plugin to enable Tapesty - a React frontend for WordPress
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+class AgreableTapestryPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableTapestryPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# agreable-tapestry-plugin
+WordPress plugin to enable Tapesty - a React frontend for WordPress
